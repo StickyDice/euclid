80d429f812765c504e17addd1ecd4c0c97266e5b
Igor Lekomtsev <leckomtsevigor@yandex.ru>
2023-04-04 20:14:37 +0300

    Accessibility added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", (e) =>{
+document.addEventListener("DOMContentLoaded", (e) => {
   // burgerMenu
   let burgerMenu = document.querySelector(".burger");
   let header = document.querySelector(".header");
@@ -32,28 +32,50 @@ document.addEventListener("DOMContentLoaded", (e) =>{
   let tabsBtn = document.querySelectorAll(".work-plan__btn");
   let tabsItem = document.querySelectorAll(".slide");
 
-  tabsBtn.forEach(function(element){
+  tabsBtn.forEach(function (element) {
     element.addEventListener("click", (el) => {
       const path = el.currentTarget.dataset.path;
 
-      tabsBtn.forEach(function(btn) {btn.classList.remove("work-plan__btn_active")});
+      tabsBtn.forEach(function (btn) { btn.classList.remove("work-plan__btn_active") });
       el.currentTarget.classList.add("work-plan__btn_active");
 
-      tabsItem.forEach(function(element) {element.classList.remove("slide_active")});
+      tabsItem.forEach(function (element) { element.classList.remove("slide_active") });
       document.querySelector(`[data-target="${path}"]`).classList.add("slide_active");
     })
   })
 
-  new Accordion(".accordion-container");
+  const acc = new Accordion(".accordion-container");
 
+  // search
   let searchBtn = document.querySelector(".header__search-btn");
   let searchForm = document.querySelector(".header__form");
+  let formSearchBtn = document.querySelector(".search-form__search-btn");
+  let searchInput = document.querySelector(".search-form__input");
   let closeBtn = document.querySelector(".search-form__close-btn");
   searchBtn.addEventListener("click", () => {
     searchForm.classList.add("search-form_active");
+    formSearchBtn.tabIndex = 0;
+    searchInput.tabIndex = 0;
+    closeBtn.tabIndex = 0;
+    searchForm.ariaHidden = false;
   });
   closeBtn.addEventListener("click", () => {
     searchForm.classList.remove("search-form_active");
+    formSearchBtn.tabIndex = -1;
+    searchInput.tabIndex = -1;
+    closeBtn.tabIndex = -1;
+    searchForm.ariaHidden = false;
+  });
+
+  // tab
+  let faqItems = document.querySelectorAll(".faq__item");
+  faqItems.forEach((item, i) => {
+    document.addEventListener("keydown", (e) => {
+      if (e.code === "Space" && document.activeElement === item) {
+        e.preventDefault();
+        acc.toggle(i);
+      }
+    });
   });
 });
 
